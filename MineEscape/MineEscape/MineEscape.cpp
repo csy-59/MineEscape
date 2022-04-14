a459235eca4243c0c32ac8495dfba60c307be63a
csy-59 <csy010509@naver.com>
2022-04-14 14:09:02 +0900

    Fix: 초기화면 추가, 종유석 추가 완료 등 추가 및 수정

--- MineEscape/MineEscape/MineEscape.cpp
+++ MineEscape/MineEscape/MineEscape.cpp
@@ -7,33 +7,61 @@ using namespace std;
 
 int main()
 {	
-	// 게임 난이도 설정 // map // sight
-	int inputGameLevel;
-	cin >> inputGameLevel;
 
-	char gameMap[42][42] = { ' ' }; //맵
+	char gameMap[42][42] = { '/' }; //맵
 	int gameMapSize; //맵 크기
 	int gameSight; //시아 크기
 
 	int gameEscape[2];	//출구 위치
 	int gamePlayerPosition[2] = { 1, 1 }; //플레이어 위치
 	char gameKey; //입력키
+	int inputGameLevel; //난이도 입력
 
-	switch (inputGameLevel)
-	{
-	case 1:
-		gameMapSize = 20;
-		gameSight = 3; // sight
-		break;
-	case 2:
-		gameMapSize = 20;
-		gameSight = 3; // sight
-		break;
-	case 3:
-		gameMapSize = 20;
-		gameSight = 3; // sight
-		break;
-	}
+	int gamesTalactiteCount; //종유석 개수
+	int gamesTalactitePosition[5][2]; //종유석 위치 배열
+
+	int gameJewelryCount; //보석 갯수
+	int gameJewelryPosition[7][2]; // 보석 위치 배열
+	int gameJewelryScore[7]; // 보석 위치 배열
+
+	int gameJewelryBasicSocre[3] = { 100,200,300 };//보석 기본 점수(여기서 랜덤으로 배정)
+
+	
+	// 게임 난이도 설정 // map // sight
+	cout << "=================" << endl;
+	cout << ">> MINE ESCAPE <<" << endl;
+	cout << "=================" << endl;
+	do {
+		cout << "난이도 입력(1~3): ";
+		cin >> inputGameLevel;
+
+		switch (inputGameLevel)
+		{
+		case 1:
+			gameMapSize = 20;
+			gamesTalactiteCount = 4;
+			gameJewelryCount = 5;
+			gameSight = 5; // sight
+			break;
+		case 2:
+			gameMapSize = 30;
+			gamesTalactiteCount = 5;
+			gameJewelryCount = 6;
+			gameSight = 4; // sight
+			break;
+		case 3:
+			gameMapSize = 40;
+			gamesTalactiteCount = 7;
+			gameJewelryCount = 7;
+			gameSight = 3; // sight
+			break;
+		default:
+			cout << "잘못된 입력" << endl;
+			break;
+		}
+	} while (inputGameLevel > 3 || inputGameLevel < 1);
+
+	cout << "로딩중..." << endl;
 
 	// 난수 생성, gameEscape(탈출구) 설정
 	//// x축
@@ -67,16 +95,6 @@ int main()
 		gameEscape[1] = rand() % gameMapSize + 1;
 	} while (gameEscape[0] == 1 && gameEscape[1] == 1);
 
-
-	// gameMap 초기화
-	for (int j = 0; j < 20; j++) // map
-	{
-		for (int i = 0; i < 20; i++) // map
-		{
-			(i == 0 || j == 0 || i == 19 || j == 19) ? gameMap[j][i] = '*' : gameMap[j][i] = '/';
-		}
-	}
-
 	// 탈출지점(gameEscape) 지정
 	gameMap[gameEscape[0]][gameEscape[1]] = 'E';
 
@@ -91,36 +109,126 @@ int main()
 		}
 	}
 
-	// 초기화면 표시
-	for (int j = 0; j < 20; j++) // map
+	// gameMap 초기화
+	for (int j = 0; j < gameMapSize + 2; j++) // map
 	{
-		for (int i = 0; i < 20; i++) // map
+		for (int i = 0; i < gameMapSize + 2; i++) // map
 		{
-			cout << gameMap[i][j];
+			(i == 0 || j == 0 || i == gameMapSize-1 || j == gameMapSize - 1) ? gameMap[j][i] = '*' : gameMap[j][i] = '/';
 		}
-		cout << endl;
 	}
 
-	// 이동 및 화면 갱신
-	while (gamePlayerPosition[0] != gameEscape[0] || gamePlayerPosition[1] != gameEscape[1])
-	{
-		gameKey = _getch();
+	//종유석 위치
+	for (int i = 0; i < gamesTalactiteCount;i++) {
+		bool isStalactitePossible = true;
+		do {
+			isStalactitePossible = true;
+			srand(time(NULL));
+			gamesTalactitePosition[i][0] = rand() % gameMapSize + 1;
+			srand(time(NULL));
+			gamesTalactitePosition[i][1] = rand() % gameMapSize + 1;
 
-		//초기화
-		gameMap[gamePlayerPosition[0]][gamePlayerPosition[1]] = ' ';
-		for (int i = -1; i < 2; i++) {
-			for (int j = -1; j < 2; j++) {
-				if (gameMap[gamePlayerPosition[0] + i][gamePlayerPosition[1] + j] == ' ')
-					gameMap[gamePlayerPosition[0] + i][gamePlayerPosition[1] + j] = '/';
+			if ((gamesTalactitePosition[i][0] == 1 && gamesTalactitePosition[i][1] == 1))
+				isStalactitePossible = false;
+
+			if ((gamesTalactitePosition[i][0] == gameEscape[0] && gamesTalactitePosition[i][1] == gameEscape[1]))
+				isStalactitePossible = false;
+
+			for (int j = 0;j < i;j++) {
+				if (gamesTalactitePosition[j][0] == gamesTalactitePosition[i][0] &&
+					gamesTalactitePosition[j][1] == gamesTalactitePosition[i][1]) {
+					//만약 종유석 위치와 겹친다면
+					isStalactitePossible = false;
+					break;
+				}
 			}
+		} while (!isStalactitePossible);
+		//종유석의 위치가 플레이어/탈출 위치/벽/다른 종유석/시아 위치가 아닐 경우 저장 완료.
+
+		//안전한 종유석의 자리라면 종유석으로 지정
+		gameMap[gamesTalactitePosition[i][0]][gamesTalactitePosition[i][1]] = 'T';
+		cout << gamesTalactitePosition[i][0] << " " << gamesTalactitePosition[i][1] << endl;
+	}
+
+	//보석 위치
+	for (int i = 0; i < gameJewelryCount;i++) {
+		bool isJewelryPossible = true;
+		do {
+			isJewelryPossible = true;
+
+			srand(time(NULL));
+			gameJewelryPosition[i][0] = rand() % gameMapSize + 1;
+			srand(time(NULL));
+			gameJewelryPosition[i][1] = rand() % gameMapSize + 1;
+
+			if ((gameJewelryPosition[i][0] == 1 && gameJewelryPosition[i][1] == 1))
+				isJewelryPossible = false;
+
+			if ((gameJewelryPosition[i][0] == gameEscape[0] && gameJewelryPosition[i][1] == gameEscape[1]))
+				isJewelryPossible = false;
+
+			for (int j = 0;j < gamesTalactiteCount;j++) {
+				if (gameJewelryPosition[i][0] == gamesTalactitePosition[j][0] &&
+					gameJewelryPosition[i][1] == gamesTalactitePosition[j][1]) {
+					//만약 종유석 위치와 겹친다면
+					isJewelryPossible = false;
+					break;
+				}
+			}
+
+			for (int j = 0;j < i;j++) {
+				if (gameJewelryPosition[j][0] == gameJewelryPosition[i][0] &&
+					gameJewelryPosition[j][1] == gameJewelryPosition[i][1]) {
+					//만약 보석 위치와 겹친다면
+					isJewelryPossible = false;
+					break;
+				}
+			}
+		} while (!isJewelryPossible);
+		//보석의 위치가 플레이어/탈출 위치/벽/다른 종유석/시아 위치가 아닐 경우 저장 완료.
+
+		//안전한 보석의 자리라면 보석으로 지정
+		gameMap[gameJewelryPosition[i][0]][gameJewelryPosition[i][1]] = 'J';
+		cout << gameJewelryPosition[i][0] << " " << gameJewelryPosition[i][1] << endl;
+
+		//보석 점수 설정
+		srand(time(NULL));
+		gameJewelryScore[i] = gameJewelryBasicSocre[rand() % 3];
+
+	}
+
+	//회복 우물 위치
+
+		bool errPosition = 0;
+	//게임
+	do {
+		// 화면 갱신
+		system("cls");
+		
+		// 초기화면 표시
+		for (int j = 0; j < gameMapSize + 2; j++) // map
+		{
+			for (int i = 0; i < gameMapSize + 2; i++) // map
+			{
+				cout << gameMap[i][j];
+			}
+			cout << endl;
 		}
+		gameKey = ' ';
+		// 이동 한계 출력
+		if (gameMap[gamePlayerPosition[0]][gamePlayerPosition[1]] == 'T' || errPosition == 1)
+			cout << "이동 불가" << endl;
+		else
+			cout << gameKey << endl;
+
+		gameKey = _getch();
+	
 		// 대소문자 무관계 인식
 		if (gameKey >= 97 && gameKey <= 122) // ASCII code a = 97, z = 122
 		{
 			gameKey -= 32;
 		}
-		bool errPosition = 0;
-		
+
 		// 플레이어 위치 변경 (switch문)
 		switch (gameKey)
 		{
@@ -174,9 +282,6 @@ int main()
 			}
 		}
 
-		// 화면 갱신
-		system("cls");
-
 		// cout << gamePlayerPosition[0] << gamePlayerPosition[1] << endl;
 
 		for (int j = 0; j < 20; j++) // map
@@ -186,10 +291,10 @@ int main()
 				cout << gameMap[i][j];
 			}
 			cout << endl;
-		}
-		// 이동 한계 출력
-		errPosition == 1 ? cout << "이동 불가" << endl : cout << gameKey << endl;
-	}
+
+		}	
+	
+	} while (gamePlayerPosition[0] != gameEscape[0] || gamePlayerPosition[1] != gameEscape[1]);
 
 	cout << "탈출에 성공했습니다." << endl;
 }
